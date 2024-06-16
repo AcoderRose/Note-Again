5093217d2c58550258848e328b0341037d561f11
AcoderRose <addyro96@att.net>
2024-06-15 21:55:15 -0700

    Made changes to notes.js file. Added POST route to save and return notes.

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,6 @@
-// Import necessary modules
+// API route handling for notes set up
+
+// Import essential modules: 'express', 'fs', 'path', and 'uuid'
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
@@ -28,8 +30,25 @@ router.get("/", async (req, res) => {
   try {
     const data = await readFromFile(path.join(__dirname, "../db/db.json"));
     res.json(JSON.parse(data));
+    // error handling
   } catch (err) {
+    //data sent as JSON
     res.status(500).json({ error: "Oops! Failed to read notes data." });
+  }
+});
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+router.post("/", async (req, res) => {
+  try {
+    const { title, text } = req.body; // title and text gotten from request body
+    const newNote = { id: uuidv4(), title, text }; // new note created with a unique ID
+    const data = await readFromFile(path.join(__dirname, "../db/db.json"));
+    const notes = JSON.parse(data); // existing notes parsed
+    notes.push(newNote); //new note added to notes array
+    await writeToFile(path.join(__dirname, "../db/db.json"), notes);
+    res.json(newNote);
+  } catch (err) {
+    res.status(500).json({ error: "Oops! Failed to save note." }); //error handling
   }
 });
 
